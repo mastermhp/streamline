60ff2ba4981233f461f7b904ff7f63f63ba36b7c
MEHEDI HASAN PARASH <112463517+mastermhp@users.noreply.github.com>
2024-11-11 01:18:57 +0600

    deployment log changed

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 import { Button } from "@/components/ui/button";
 import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
-import HeroSection1 from "./homepage/_components/heroSection1";
+import HeroSection1 from "./homepage/_components/HeroSection1";
 import StatsSection from "./homepage/_components/StatsSection";
 import CtaSection from "./homepage/_components/CtaSection";
 import BentoGridSection from "./homepage/_components/BentoGridSection";
